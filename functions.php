0326a56eab13f88877c686178973fb0b7f9be0c2
Angella <>
2019-05-07 17:09:38 +0100

    DEV - Creation of custom post types (destination, accommodation and package)

--- functions.php
+++ functions.php
@@ -362,21 +362,20 @@ add_filter('template_include', 'template_chooser');
 **********************/
 function reorder_admin_menu( $__return_true ) {
     return array(
-'index.php', // Dashboard
-'edit.php?post_type=job', // Jobs
-'edit.php?post_type=candidate', // Candidate
-'separator1', // --Space--
-'edit.php?post_type=page', // Pages 
-'separator2', // --Space--
-'upload.php', // Media
-'separator2', // --Space--
-'themes.php', // Appearance
-'plugins.php', // Plugins
-'users.php', // Users
-'tools.php', // Tools
-'options-general.php', // Settings
-'separator3', // --Space--
-'wpcf7', // Contact Form 7 
+		'index.php',                        // Dashboard
+		'edit.php?post_type=accommodation', // Accommodations
+		'edit.php?post_type=destination',   // Destinations
+		'edit.php?post_type=package',       // Package
+		'separator1',                       // --Space--
+		'edit.php?post_type=page',          // Pages
+		'upload.php',                       // Media
+		'separator2',                       // --Space--
+		'themes.php',                       // Appearance
+		'plugins.php',                      // Plugins
+		'users.php',                        // Users
+		'tools.php',                        // Tools
+		'options-general.php',              // Settings
+		'wpcf7',                            // Contact Form 7 
 
    );
 }
