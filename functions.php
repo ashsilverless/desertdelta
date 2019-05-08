0378d7366df7da21c6eb481d668fa8a5bbff4791
Angella <>
2019-05-08 13:44:35 +0100

    DEV - Creation of News page and carousel for the latest news

--- functions.php
+++ functions.php
@@ -360,6 +360,7 @@ function reorder_admin_menu( $__return_true ) {
 		'index.php',                        // Dashboard
 		'edit.php?post_type=accommodation', // Accommodations
 		'separator1',                       // --Space--
+		'edit.php',                         // Posts
 		'edit.php?post_type=page',          // Pages
 		'upload.php',                       // Media
 		'separator2',                       // --Space--
@@ -380,7 +381,6 @@ add_filter( 'menu_order', 'reorder_admin_menu' );
 * Remove top level and sub menu admin menus
 */
 function remove_admin_menus() {
-   remove_menu_page( 'edit.php' ); // Posts 
    remove_menu_page( 'edit-comments.php' ); // Comments
    remove_menu_page( 'tools.php' ); // Tools
 }
--- template-parts/hero.php
+++ template-parts/hero.php
@@ -13,33 +13,22 @@
     <div class="hero <?php the_field( 'hero_height' );?>" style="background-image: url(<?php echo $heroImage['url']; ?>); background-color: <?php echo $heroColor; ?>;">
 
     <div class="container">
-    
-        <div class="row">
                 
-            <div class="hero__content">       
+        <div class="hero__content">       
 
-        				    <?php $brandImage = get_field('logo', 'options');?>	
-        				
-    						<a href="<?php echo home_url(); ?>" alt="<?php wp_title(''); ?>" title="<?php wp_title(''); ?>" class="logo">
-        						
-        						<?php get_template_part('template-parts/logo');?>
-        						
-    						</a>
+		    <?php $brandImage = get_field('logo', 'options');?>	
+		
+			<a href="<?php echo home_url(); ?>" alt="<?php wp_title(''); ?>" title="<?php wp_title(''); ?>" class="logo">
+				
+				<?php get_template_part('template-parts/logo');?>
+				
+			</a>
 
-                <h1 class="heading heading__xl heading__light center slow-fade"><?php the_field( 'hero_heading' );?></h1>
-
-                <?php if ( is_front_page() ) {?>
-                
-	<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" class="search-form mt3 mb3">
-		<input type="text" class="field" name="s" id="s" placeholder="<?php esc_attr_e( 'Job Title / Job Keywords', 'desertdelta' ); ?>" />
-		<input type="hidden" name="post_type" value="job" />
-		<input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search Jobs', 'desertdelta' ); ?>" />
-	</form>
-
-                <?php }?>
-           
-            </div>       
-                
+			<div class="hero__text">
+	            <h1 class="heading heading__xl heading__light center slow-fade"><?php the_field( 'hero_heading' );?></h1>
+				<h2 class="heading heading__lg heading__light center slow-fade"><?php the_field( 'hero_sub_heading' );?></h2>
+			</div>
+			
         </div>
     
     </div>
