57627a98aa391ec9c2fc5775631bc38eaffb345d
Angella <>
2019-05-17 17:14:26 +0100

    DEV - Minor adjustments on layout

--- template-parts/cta-newsletter.php
+++ template-parts/cta-newsletter.php
@@ -14,7 +14,7 @@ while (have_rows('cta_newsletter', 'options')) : the_row();?>
             
         </div>
         
-        <div class="form">
+        <div class="form mt2">
             
             <?php echo do_shortcode('[contact-form-7 id="2157" title="Newsletter Form"]');?>
 
